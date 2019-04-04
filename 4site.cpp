1afc8fa8f1384b108b5211192925f283b7dfb473
Abhishek Kumar <abhi.kumar@niser.ac.in>
2019-04-04 08:32:50 +0530

    Array added

--- 4site.cpp
+++ 4site.cpp
@@ -12,11 +12,12 @@ class Basis
     int L;
 
     int create()
-    {       
+    {   int binary[1<<(2*L)][(2*L)] = {0};
+        int k = 0;   
         for(int N=0;N<=2*L;N++)
         {   if ( N == 0)
             {    
-                int binaryNum[2*L+1] = {0}; 
+                int binaryNum[2*L] = {0}; 
                 cout <<"Basis Decimal Representation:" << 0 << "\n";
                 cout <<"Basis Binary Representation:" <<"\n";
                 for (int j = (2*L)-1; j >= 0; j--) 
@@ -26,13 +27,14 @@ class Basis
                     
             }
             else
-            {   int n_min = 0;
-                int n_max = 0;
-                for (int j=0;j<=N;j++)
-                {
-                    n_min = n_min + pow(2,j) ;
-                    n_max = n_max + pow(2,(2*L - j)) ;
-                }
+            {   int n_min = (1<<(N))-1;
+                int n_max = (1<<(2*L))-(1<<(2*L-N));
+                //for (int j=0;j<N;j++)
+                //{
+                    //n_min = n_min + pow(2,j) ;
+                    //n_max = n_max + pow(2,(2*L-1 - j)) ;
+                //}
+                
                 for (int i=n_min;i<=n_max;i++)
                 {   
                 
@@ -50,39 +52,41 @@ class Basis
                     {      
                         sum = sum + binaryNum[j]; 
                     }
-  
+                
                     if (sum == N)
-                    {   cout <<"Basis Decimal Representation:" << i << "\n";
+                    {   cout <<"\n"<<"Basis Decimal Representation:" << i << "\n";
                         cout <<"Basis Binary Representation:" <<"\n";
+                        
                         for (int j = (2*L)-1; j >= 0; j--) 
-                        {     
-                            cout << binaryNum[j] ; 
-                            //for (int k = )
+                        {   
+                            binary[k][j] = binaryNum[j];
+                            cout << binaryNum[j] ;     
                         }
-                        //int binary[2**16] = {0}
-                        //binary[i]=
+                        k++;
                     }
-
                 }
-
             }
+        }
+    }
+};
 
+class c_dag
+{
+    public:
+    int spin, site, old_basis[][] ;
+    int action()
+    {
+        if (spin=up && site <= L)
+        {  for() 
+            int new_basis[t][site-1] = 
+        }
+        if (spin=down && site <= L)
+        {
 
         }
-        
-
-
     }
 
 };
-
-//class c_dag
-//{
-    //public:
-    //int binary ;
-
-
-//}
 int main()
 {
     Basis obj1;
